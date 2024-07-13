263d441121278b710f37200f6dddb74392c88219
Roselande Woods <168475003+roselandewoods@users.noreply.github.com>
2024-07-12 22:18:03 -0400

    Update dom.js

--- dom.js
+++ dom.js
@@ -1,14 +1,13 @@
-export function displayWeather(weather) {
+export const displayWeather = (weather) => {
     const weatherResult = document.getElementById('weather-result');
     weatherResult.innerHTML = `
         <h2>${weather.name}</h2>
         <p>${weather.weather[0].description}</p>
         <p>${weather.main.temp} °F</p>
-        
     `;
-}
+};
 
-export function showError(message) {
+export const showError = (message) => {
     const weatherResult = document.getElementById('weather-result');
     weatherResult.innerHTML = `<p>${message}</p>`;
-}
+};
